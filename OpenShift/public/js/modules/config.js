1f6c6ec9c5bd1c5a5aa2ebbaa405562e5c5d5fb8
Jak71004 <Jonathan@CandiedYamPublishing.com>
2016-03-18 15:42:55 -0400

    Create config.js

---
+++ OpenShift/public/js/modules/config.js
@@ -0,0 +1,35 @@
+(function ( ) {
+    angular.module('BetYouFail')
+        .config(function ($routeProvider) {
+        $routeProvider.
+            when('/home', {
+                templateUrl: 'home.html',
+                //controller: 'mainCtrl'
+            }).
+            when('/wager', {
+                templateUrl: 'wager.html',
+                //controller: 'mainCtrl'
+            }).            
+            when('/defeat', {
+                templateUrl: 'defeat.html',
+                controller: 'defeatCtrl'
+            }).
+            when('/faq', {
+                templateUrl: 'faq.html',
+                //controller: 'faqCtrl'
+            }).
+            when('/contactus', {
+                templateUrl: 'contactus.html',
+                //controller: 'contactCtrl'
+            }).
+            when('/failure', {
+                templateUrl: 'failure.html',
+                //controller: 'mainCtrl'
+            }).
+            when('/success', {
+                templateUrl: 'success.html',
+                //controller: 'mainCtrl'
+            }).
+            otherwise({redirectTo:'/home'});
+        });
+}());
